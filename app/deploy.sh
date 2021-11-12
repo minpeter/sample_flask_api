1d5b5c5abc75a6ac4be17b6a1f55d2888edbc029
minpeter <kali2005611@gmail.com>
2021-11-12 15:53:36 +0900

    deploy edit

--- app/deploy.sh
+++ app/deploy.sh
@@ -5,7 +5,7 @@ REPOSITORY=/home/ubuntu/build
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl action | grep python | awk '{print $1}')
+CURRENT_PID=$(ps -ef | grep python | awk '{print $2}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
