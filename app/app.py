8b95ba679ea4ab5b2bf953366b06d86c6c5f6725
minpeter <kali2005611@gmail.com>
2022-05-04 15:52:53 +0900

    New composition after some study

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-# import ssl
 
 app = Flask(__name__)
 CORS(app, resources=r'/*')
@@ -28,6 +27,4 @@ def egg():
     return jsonify({"msg": "🥚"})
 
 if __name__ == '__main__':
-    # ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    # ssl_context.load_cert_chain(certfile='cert.pem', keyfile='key.pem', password='louie')
-    app.run(host='0.0.0.0', port="5000") #, ssl_context=ssl_context
+    app.run(host='0.0.0.0', port="5000")
